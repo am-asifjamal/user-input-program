4e62d13963d37c0cc7ea960065c58bc00407bf7a
Asif jamal <amasifjamal@gmail.com>
2026-06-05 12:43:42 +0530

    user-input-program.py
    A simple Python program that takes user input such as name, age, and city and displays the information.

---
+++ .py
@@ -0,0 +1,14 @@
+# user-input-program
+#A simple Python program that takes user input such as name, age, and city and displays the information.
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+city = input("Enter your city: ")
+mobile = input("Enter your mobile number: ")
+email = input("Enter your email: ")
+college = input("Enter your college name: ")
+print(name)
+print(age)
+print(city)
+print(mobile)
+print(email)
+print(college)
--- README.md
+++
@@ -1,2 +0,0 @@
-# user-input-program
-A simple Python program that takes user input such as name, age, and city and displays the information.
